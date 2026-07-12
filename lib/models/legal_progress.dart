54cc8be373c25bf4918420159af518aa17d56af1
liarahmanrangkuti <liarahmanrangkuti@gmail.com>
2026-07-12 21:25:47 +0700

    Add legal_progress

---
+++ lib/models/legal_progress.dart
@@ -0,0 +1,17 @@
+class LegalProgress {
+  final int id;
+  final String companyName;
+  final String clientName;
+  final String email;
+  final int percentage;
+  final String status;
+
+  const LegalProgress({
+    required this.id,
+    required this.companyName,
+    required this.clientName,
+    required this.email,
+    required this.percentage,
+    required this.status,
+  });
+}
